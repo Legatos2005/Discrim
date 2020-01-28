c2929f6c728a61fe92a26b8c66fc590126b06d7e
Legatos2005 <59206177+Legatos2005@users.noreply.github.com>
2020-01-28 09:59:20 +0200

    Add files via upload

---
+++ Homework.js
@@ -0,0 +1,26 @@
+newFunction();
+
+function newFunction() {
+    var vopros = confirm('Будем решать квадратное уравнение?');
+    console.log(vopros);
+    if (vopros) {
+        var a = prompt('Введите первое число', '');
+        var b = prompt('Введите второе число', '');
+        var c = prompt('Введите третье число', '');
+        var discrim = (b * b - 4 * a * c);
+        if (discrim > 0) {
+            x1 = (-b + Math.sqrt(discrim)) / (2 * a);
+            x2 = (-b - Math.sqrt(discrim)) / (2 * a);
+            alert('Первый корень = ' + x1 + ' а второй корень = ' + x2);
+        }
+        if (discrim == 0) {
+            x = (-b) / (2 * a);
+            alert('У нас всего 1 корень x = ' + x);
+        }
+        if (discrim < 0) {
+            alert('К сожалению корней нет');
+        }
+    } else {
+        alert("Заходите в другой раз ;)");
+    }
+}
